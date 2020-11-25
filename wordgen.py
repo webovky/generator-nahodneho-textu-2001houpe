c699e63d1db6530eeae6ea1253fabcc793e464c5
2001houpe <71071656+2001houpe@users.noreply.github.com>
2020-11-25 15:22:41 +0100

    Generátor textu - reupload
    Omylem jsem si smazal repositář s původním úkolem.

--- wordgen.py
+++ wordgen.py
@@ -1,14 +1,41 @@
 #!/usr/bin/env python3
 
-from random import choice, randint
+import random
 
-samohlasky = "aeiouy"
-souhlasky = "qwrtpsdfghjklzxcvbnm"
+try:  
+    filename = input("Zadejte jméno souboru:")
+    with open(filename + ".txt","w", encoding = "utf-8" ) as soubor:
+        abeceda = "qwertzuiopasdfghjklyxcvbnm"
+        pslov = int(input("Zadejte počet slov:"))
+        if pslov <0:
+            print("Příště zadejte přirozené číslo!")
+        pslov = abs(pslov)
+        count = 1
+        pocet_znaku = 0
+        while (count <= pslov):
+            delka_slova= random.randint(1,7)
+            slovo = ""
+            for a in range(delka_slova):
+                pismeno = random.choice(abeceda)
+                slovo = slovo + pismeno
+                pocet_znaku = pocet_znaku + 1
+            soubor.write(slovo)
+            if (count < pslov):
+                soubor.write(" ")
+            else:
+                break
+            pocet_znaku = pocet_znaku + 1
+            count = count + 1
+            if (pocet_znaku > 80):
+                soubor.write("\n")
+                pocet_znaku = 0
+            else:
+                continue
+except ValueError :
+    print("Špatně jsi zadal počet slov. Zkus příště zadat přirozené číslo.") 
+except type(float) :
+    print("Špatně jsi zadal počet slov. Zkus příště zadat přirozené číslo.")
 
-pslov = int(input("počet slov  > "))
-filename = input("jméno souboru  > ")
 
-try:
-    pass
-except:
-    pass
+
+                 
